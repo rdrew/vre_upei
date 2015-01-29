b6844060b233c56c98d31b7d78cf3b82b0a45fe9
rdrew <robdrewupei@gmail.com>
2015-01-29 00:18:45 -0400

    upei vre 2015
    ver theme based on at-commerce

---
+++ template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "vre_upei" to match
+ * your subthemes name, e.g. if you name your theme "vre_upei" then the function
+ * name will be "vre_upei_preprocess_hook". Tip - you can search/replace
+ * on "vre_upei".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function vre_upei_preprocess_html(&$vars) {
+}
+function vre_upei_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function vre_upei_preprocess_page(&$vars) {
+}
+function vre_upei_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function vre_upei_preprocess_node(&$vars) {
+}
+function vre_upei_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function vre_upei_preprocess_comment(&$vars) {
+}
+function vre_upei_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function vre_upei_preprocess_block(&$vars) {
+}
+function vre_upei_process_block(&$vars) {
+}
+// */
